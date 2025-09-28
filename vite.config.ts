68d8db69524565981a6a66fd553ce74b8ee5a210
Mahmoud Metwaly <xischemmetwal@Mahmoud-Metwally.local>
2025-09-28 18:38:06 +0400

    fix: remove backslash

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/social-support-app/",
+  base: "/social-support-app",
 
   server: {
     port: 3000,
