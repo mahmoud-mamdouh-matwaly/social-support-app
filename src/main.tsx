314fa688404e85fd4f41a35320d65251684b3779
Mahmoud Metwaly <xischemmetwal@Mahmoud-Metwally.local>
2025-09-29 23:55:34 +0400

    netlify

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./store/index.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/social-support-app">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
